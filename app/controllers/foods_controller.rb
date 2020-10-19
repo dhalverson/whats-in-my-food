21d2042c547f6b5b1b040022c02b1f2fae169bbb
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-10-19 10:22:48 -0600

    API key not working in ENV

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,25 +2,13 @@ class FoodsController < ApplicationController
   def index
     keywords = params[:q]
     conn = Faraday.new(url: 'https://api.nal.usda.gov/fdc/v1/')
-    response = conn.get("foods/search?query=#{keywords}&api_key=#{ENV['API_KEY']}")
-
-
+    response = conn.get("foods/search?api_key=45tec3G0DB9wkPXVf8ni2qwpKriipSJsHljo6MCo&query=#{keywords}")
+    @foods = JSON.parse(response.body, symbolize_names: true)
 
     require 'pry'; binding.pry
   end
 
-  def show
-    require 'pry'; binding.pry
-  end
+
 end
 
 
-
-  # conn = Faraday.new(url: 'https://api.nal.usda.gov/fdc/v1/')
-  # response = conn.get ("foods?fdcIds=534358,534350&api_key=45tec3G0DB9wkPXVf8ni2qwpKriipSJsHljo6MCo")
-  # @food = JSON.parse(response.body, symbolize_names: true)
-
-
-  # https://api.nal.usda.gov/fdc/v1/foods/search?query=cheddar%20cheese
-
-  # https://api.nal.usda.gov/fdc/v1/foods/search?query=cheddar%20cheese&api_key=45tec3G0DB9wkPXVf8ni2qwpKriipSJsHljo6MCo
